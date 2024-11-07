e9924abec24ee55f76e153271f8de5a8c708112e
nu20 <bibhutipatra123@gmail.com>
2024-11-07 18:43:43 +0530

    first commit

---
+++ len.py
@@ -0,0 +1,4 @@
+list = ["spider man home coming" , "spider man far from home" , "spiderman no way home" , "spiderman into the spider verse" , "spiderman across the spider verse" , "the amazing spider man" , "spiderman 3"]
+print(list)
+print(list[3])
+print(list[5])
---
+++ list.py
@@ -0,0 +1,7 @@
+superheros = ["bat man" , "spider man" , "iron man" , "thor"]
+print(superheros[1:3])
+print(superheros[1:])
+
+
+empty_list = []
+print(empty_list)
